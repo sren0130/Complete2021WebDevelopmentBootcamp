bfb0917de774795819e9e4e8364fd9692899f695
sren0130 <70994978+sren0130@users.noreply.github.com>
2021-10-03 13:01:40 -0400

    Initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Heading from "./heading";
+import List from "./list";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
